840c1bc7cbbd58e25043fd95396b3fbb2e0ad355
Travis Swift <travis.arthur.swift@gmail.com>
2022-03-05 22:49:56 -0600

    added event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var buttonEl = document.querySelector("#save-task"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("i");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-  alert("button clicked");
-});
+var createTaskHandler = function() { 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-var createTaskHandler = function(event) {
-  
-    event.preventDefault();
-
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
-
-formEl.addEventListener("click", createTaskHandler);
-
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
- 
-
- 
+buttonEl.addEventListener("click", createTaskHandler);
